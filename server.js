306ab137d74fb33ffec27c8693ef1e6f8d2b5743
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-08 14:26:26 +0000

    refactor api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,41 +19,26 @@ app.get('/todos', function (req,res) {
 
 app.get('/todos/:id', function (req,res) {
 	var todoId = parseInt(req.params.id,10);
-
+	var matchedTodo = _.findWhere(todos,{id: todoId});
 	
-	if(todoId > todos.length) {
-		res.status(404).send();
-		console.log(todos);
-
+	if(matchedTodo) {
+		res.json(matchedTodo);
 	} else {
-		for (var i = 0; i < todos.length; i++){
-			if(todoId === todos[i].id) {
-				res.json(todos[i]);
-			}
-		}
+		res.status(404).send();
 	}
-	
-	//res.send('Asking for todo with id of ' + req.params.id);
 });
 
 app.post('/todos', function (req,res){
-	
-	var body = req.body;
+	var body = _.pick(req.body, 'description' , 'completed');
+	console.log()
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
-	// var object = {
-	// 	id : todoNextId,
-	// 	description : body.description,
-	// 	completed : body.completed,
-	// };
-
-	// todoNextId++;
 	todos.push(body);
-	//console.log(todos);
-	
-	//console.log('description: ' + body.description);
-	//console.log(todos.length);
-	res.status(200).send();
-	//res.json(body);
+	//res.status(200).send();
+	res.json(body);
 });
 
 app.listen(PORT, function(){
