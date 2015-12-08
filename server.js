91308a838a6305e5a2a095c0bf5d1eca1aa59a21
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-08 11:59:22 +0000

    add POST for todo

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos =[{
-	id: 1,
-	description: 'Meet mum for lunch.',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market.',
-	completed: false
-}, {
-	id: 3,
-	description: 'Call friend about staying.',
-	completed: true
-}]
+var todos =[]
+var todoNextId = 1;
 
+app.use(bodyParser.json())
 
 app.get('/', function (req,res){
 	res.send('Todo API Root');
@@ -27,8 +19,11 @@ app.get('/todos', function (req,res) {
 app.get('/todos/:id', function (req,res) {
 	var todoId = parseInt(req.params.id,10);
 
+	
 	if(todoId > todos.length) {
 		res.status(404).send();
+		console.log(todos);
+
 	} else {
 		for (var i = 0; i < todos.length; i++){
 			if(todoId === todos[i].id) {
@@ -38,6 +33,26 @@ app.get('/todos/:id', function (req,res) {
 	}
 	
 	//res.send('Asking for todo with id of ' + req.params.id);
+});
+
+app.post('/todos', function (req,res){
+	
+	var body = req.body;
+	body.id = todoNextId++;
+	// var object = {
+	// 	id : todoNextId,
+	// 	description : body.description,
+	// 	completed : body.completed,
+	// };
+
+	// todoNextId++;
+	todos.push(body);
+	//console.log(todos);
+	
+	//console.log('description: ' + body.description);
+	//console.log(todos.length);
+	res.status(200).send();
+	//res.json(body);
 });
 
 app.listen(PORT, function(){
