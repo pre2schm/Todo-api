e6420e109a67bbc0b9ce777248c417c3c47a91eb
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-09 22:50:14 +0000

    Add Put

--- server.js
+++ server.js
@@ -50,8 +50,34 @@ app.delete('/todos/:id' , function (req, res){
 		//res.status(200).send();
 		res.json(matchedTodo);
 	} else {
-		res.status(404).send();
+		res.status(404).json({"error" : "no todo found with that id"});
 	}
+});
+
+app.put('/todos/:id', function (req,res){
+	var body = _.pick(req.body, 'description' , 'completed');
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos,{id: todoId});
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+	
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function(){
