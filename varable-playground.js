10bd86b8e2acee378742b6c6359232fe4d928d24
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-15 21:50:04 +0000

    add variable playground

---
+++ varable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+// 	name: 'Sam',
+// 	age: 30
+// }
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Sam',
+// 	// 	age: 31
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades =[15, 88];
+
+function addGrades (gradesArr){
+	grades.push(55);
+	debugger;
+	//gradesArr = [12,13,33];
+}
+
+addGrades(grades);
+console.log(grades);
