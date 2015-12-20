329590166fb13edc81ea1928a6fd5a4956d2ae7c
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-20 21:33:35 +0000

    GET todos by id

--- server.js
+++ server.js
@@ -35,14 +35,18 @@ app.get('/todos', function (req,res) {
 });
 
 app.get('/todos/:id', function (req,res) {
-	var todoId = parseInt(req.params.id,10);
-	var matchedTodo = _.findWhere(todos,{id: todoId});
-	
-	if(matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	 var todoId = parseInt(req.params.id,10);
+
+	 db.todo.findById(todoId).then(function(todo){
+	 	
+	 	if(todo){
+	 		res.json(todo.toJSON());
+	 	} else {
+	 		res.status(404).json('no todo found');
+	 	}
+	 }, function (e) {
+	 	res.status(500).send();
+	 });
 });
 
 app.post('/todos', function (req,res){
