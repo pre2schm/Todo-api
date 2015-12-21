8a98f78f0d94cdb469142462c7138396c1cd2d47
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-21 16:37:18 +0000

    Convert DELETE todos to sequelize

--- server.js
+++ server.js
@@ -74,15 +74,38 @@ app.post('/todos', function (req,res){
 
 app.delete('/todos/:id' , function (req, res){
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo = _.findWhere(todos,{id: todoId});
+	
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function () {
+		res.status(500).send();
+	});
 
-	if(matchedTodo){
-		todos = _.without(todos,matchedTodo);
-		//res.status(200).send();
-		res.json(matchedTodo);
-	} else {
-		res.status(404).json({"error" : "no todo found with that id"});
-	}
+	// db.todo.findById(todoId).then(function(todo){
+		 	
+	// 	 	if(todo){
+	// 	 		res.json(todo.toJSON());
+	// 	 		return todo.destroy();
+
+	// 	 	} else {
+	// 	 		res.status(404).json({
+	// 	 			error: 'no todo found'
+	// 	 		});
+	// 	 	}
+	// 	 }, function (e) {
+	// 	 	res.status(500).send();
+	// 	 });
+
 });
 
 app.put('/todos/:id', function (req,res){
