3d374f9a2f3d30d4e99eec2e5ab390e0ff1a5576
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-21 20:46:40 +0000

    convert email to lower case

--- models/user.js
+++ models/user.js
@@ -14,6 +14,15 @@ module.exports = function(sequelize, DataTypes) {
 			validate: {
 				len: [7,100]
 			}
+		} 
+	},	{
+		hooks: {
+			beforeValidate: function(user, options){
+				if(typeof user.email === 'string'){
+					user.email = user.email.toLowerCase();
+					
+				}
+			}
 		}
 	});
 }
