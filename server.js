2b11efb05b10ad122869eb2e4c02442a93031642
Samuel Morris <samuel.morris@yahoo.co.uk>
2015-12-25 17:15:42 +0000

    convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', middleware.requireAuthentication, function (req,res){
 
 app.get('/todos', middleware.requireAuthentication, function (req,res) {
 	var query = req.query;
-	var where ={};
+	var where ={
+		userId: req.user.get('id')
+	};
 
 	if(query.hasOwnProperty('completed') && query.completed === 'true'){
 		where.completed = true;
@@ -33,6 +35,8 @@ app.get('/todos', middleware.requireAuthentication, function (req,res) {
 		};
 	}
 
+	
+
 	db.todo.findAll({where:where}).then(function(todos){
 		res.json(todos);
 	}, function (e) {
@@ -44,8 +48,13 @@ app.get('/todos', middleware.requireAuthentication, function (req,res) {
 
 app.get('/todos/:id', middleware.requireAuthentication, function (req,res) {
 	 var todoId = parseInt(req.params.id,10);
-
-	 db.todo.findById(todoId).then(function(todo){
+	 
+	 db.todo.findOne({
+	 	where: {
+	 		id: todoId,
+	 		userId: req.user.get('id')
+	 		}
+	 	}).then(function(todo){
 	 	
 	 	if(todo){
 	 		res.json(todo.toJSON());
@@ -79,7 +88,8 @@ app.delete('/todos/:id' , middleware.requireAuthentication, function (req, res){
 	
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function (rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -110,8 +120,13 @@ app.put('/todos/:id', middleware.requireAuthentication, function (req,res){
 		attributes.description = body.description;
 	} 
 	
-	db.todo.findById(todoId).then(function (todo){
-		if(todo){
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+			}
+		}).then(function (todo){
+		if(todo && todo.userId === req.user.get('id')){
 			todo.update(attributes).then(function (todo){
 			res.json(todo.toJSON());
 		}, function (e) {
